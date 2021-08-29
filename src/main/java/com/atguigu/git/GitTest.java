b41ee92c7bc6ad38aaf623e484211e4a661543af
zhangyanhua <1242904793@qq.com>
2021-08-29 16:32:50 +0800

    first commit idea git 测试

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Description: GitTest
+ * @Author Zhangyanhua
+ * @Date: 2021/8/29 16:27
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
