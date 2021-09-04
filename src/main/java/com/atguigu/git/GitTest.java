b0ef893baf1174b0be9e079179e33e1482fc15ea
zyh1990913 <47167414+zyh1990913@users.noreply.github.com>
2021-09-04 17:10:55 +0800

    在线添加

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("在线添加");
     }
 }
